51d659845dc6f8196baebf1d29e4a15a1cfb7d08
Iszuddin Ismail <dino@websegera.my>
2025-04-02 12:11:39 +0800

    exclude the role of administrator, shop_manager and editor from the option
    Signed-off-by: Iszuddin Ismail <dino@websegera.my>

--- woop-customer-roles.php
+++ woop-customer-roles.php
@@ -6,15 +6,19 @@ Version: 1.0
 Author: Iszuddin Ismail
 */
 
+// Define excluded roles as a constant
+define( 'WOOP_CUSTOMER_ROLES_EXCLUDED', array( 'administrator', 'shop_manager', 'editor' ) );
+
 // Add a new tab to product data tabs
 function woop_customer_roles_product_tab( $tabs ) {
-    // Add a new tab with a title and a callback function to render its content
-    $tabs['woop-customer-roles'] = array(
-        'label'     => __( 'Grant Roles', 'woop-customer-roles' ),
-        'target'    => 'woop-customer-roles',
-        'class'     => array( 'show_if_simple', 'show_if_variable' ), // Show tab for simple and variable products
-        'icon'      => 'dashicons-admin-users', // Change the icon here
-    );
+    if ( current_user_can( 'manage_woocommerce' ) ) { // Restrict to Administrator and Shop Manager
+        $tabs['woop-customer-roles'] = array(
+            'label'     => __( 'Grant Roles', 'woop-customer-roles' ),
+            'target'    => 'woop-customer-roles',
+            'class'     => array( 'show_if_simple', 'show_if_variable' ),
+            'icon'      => 'dashicons-admin-users',
+        );
+    }
     return $tabs;
 }
 add_filter( 'woocommerce_product_data_tabs', 'woop_customer_roles_product_tab' );
@@ -25,47 +29,48 @@ function woop_customer_roles_tab_content() {
     $user_roles = get_post_meta( $post->ID, 'woop_customer_roles_user_roles', true );
     $user_roles = ! empty( $user_roles ) ? (array) $user_roles : array();
 
-    $all_roles = wp_roles()->roles;    
-
-    // Roles to exclude
-    $excluded_roles = array( 'administrator', 'shop_manager', 'editor' );
+    $all_roles = wp_roles()->roles;
 
     // Filter out excluded roles
-    foreach ( $excluded_roles as $role_key ) {
+    foreach ( WOOP_CUSTOMER_ROLES_EXCLUDED as $role_key ) {
         unset( $all_roles[ $role_key ] );
-    }    
+    }
 
     $role_names = array_column($all_roles, 'name');
-    array_multisort($role_names, SORT_ASC, $all_roles);    
+    array_multisort($role_names, SORT_ASC, $all_roles);
     ?>
-
-<div id="woop-customer-roles" class="panel woocommerce_options_panel">
-
-<div class="options_group">
-        <p class="description"><?php _e( 'Select roles to be granted when this product is purchased.', 'woop-customer-roles' ); ?></p>
-
-        <p class="form-field">
-            <label for="woop_customer_roles_user_roles"><?php _e( 'User Roles To Be Granted:', 'woop-customer-roles' ); ?></label>
-            <select name="woop_customer_roles_user_roles[]" id="woop_customer_roles_user_roles" multiple="multiple" style="width: 100%;">
-                <?php
-                foreach ( $all_roles as $role_key => $role_value ) {
-                    $selected = in_array( $role_key, $user_roles ) ? 'selected="selected"' : '';
-                    echo '<option value="' . esc_attr( $role_key ) . '" ' . $selected . '>' . esc_html( $role_value['name'] ) . '</option>';
-                }
-                ?>
-            </select>
-        </p>
+    <div id="woop-customer-roles" class="panel woocommerce_options_panel">
+        <div class="options_group">
+            <p class="description"><?php _e( 'Select roles to be granted when this product is purchased.', 'woop-customer-roles' ); ?></p>
+            <p class="form-field">
+                <label for="woop_customer_roles_user_roles"><?php _e( 'User Roles To Be Granted:', 'woop-customer-roles' ); ?></label>
+                <select name="woop_customer_roles_user_roles[]" id="woop_customer_roles_user_roles" multiple="multiple" style="width: 100%;">
+                    <?php
+                    foreach ( $all_roles as $role_key => $role_value ) {
+                        $selected = in_array( $role_key, $user_roles ) ? 'selected="selected"' : '';
+                        echo '<option value="' . esc_attr( $role_key ) . '" ' . $selected . '>' . esc_html( $role_value['name'] ) . '</option>';
+                    }
+                    ?>
+                </select>
+            </p>
+        </div>
     </div>
-
-</div>
     <?php
 }
 add_action( 'woocommerce_product_data_panels', 'woop_customer_roles_tab_content' );
 
 // Save the custom tab data
 function woop_customer_roles_save_product_tab_data( $post_id ) {
+    if ( ! current_user_can( 'manage_woocommerce' ) ) { // Restrict to Administrator and Shop Manager
+        return;
+    }
+
     $user_roles = isset( $_POST['woop_customer_roles_user_roles'] ) ? $_POST['woop_customer_roles_user_roles'] : array();
     $user_roles = array_map( 'sanitize_text_field', $user_roles );
+
+    // Exclude restricted roles
+    $user_roles = array_diff( $user_roles, WOOP_CUSTOMER_ROLES_EXCLUDED );
+
     update_post_meta( $post_id, 'woop_customer_roles_user_roles', $user_roles );
 }
 add_action( 'woocommerce_process_product_meta', 'woop_customer_roles_save_product_tab_data' );
@@ -96,6 +101,11 @@ function woop_customer_roles_grant_user_roles_on_purchase( $order_id ) {
 
             // Grant user roles to the customer
             foreach ( $product_user_roles as $role ) {
+
+                if(in_array($role, WOOP_CUSTOMER_ROLES_EXCLUDED)) {
+                    continue; // Skip excluded roles
+                }
+
                 $user->add_role( $role );
             }
         }
